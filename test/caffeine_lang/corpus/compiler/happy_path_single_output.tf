232560cb28e54778f1e1ef132f74d552368880c8
rob durst <me@robdurst.com>
2025-12-29 10:27:23 -0400

    2.0.4 release

--- test/caffeine_lang/corpus/compiler/happy_path_single_output.tf
+++ test/caffeine_lang/corpus/compiler/happy_path_single_output.tf
@@ -23,7 +23,7 @@ variable "datadog_app_key" {
 
 resource "datadog_service_level_objective" "acme_slos_checkout_availability" {
   name = "checkout_availability"
-  tags = ["managed_by:caffeine", "caffeine_version:2.0.3", "org:acme", "team:payments", "service:slos", "blueprint:api_availability", "expectation:checkout_availability", "artifact:SLO", "env:production", "vendor:datadog"]
+  tags = ["managed_by:caffeine", "caffeine_version:2.0.4", "org:acme", "team:payments", "service:slos", "blueprint:api_availability", "expectation:checkout_availability", "artifact:SLO", "env:production", "vendor:datadog"]
   type = "metric"
 
   query {
