617f4d0e5701a2f4967751e9e0fcffce8f1b4903
rob durst <me@robdurst.com>
2026-01-24 09:32:39 -0700

    fix: watch.sh had issues when one of them failed (now that we have a monolith with "three submodules"

--- watch.sh
+++ watch.sh
@@ -5,12 +5,16 @@ echo "Watching caffeine_lang/, caffeine_cli/, and caffeine_lsp/ for changes..."
 echo "Press Ctrl+C to stop"
 
 # Run tests once at startup
-cd caffeine_lang && gleam test "$@" && cd ../caffeine_lsp && gleam test "$@" && cd ../caffeine_cli && gleam test "$@" && cd ..
+(cd caffeine_lang && gleam test "$@")
+(cd caffeine_lsp && gleam test "$@")
+(cd caffeine_cli && gleam test "$@")
 
 # Watch for changes in all packages
 fswatch -o caffeine_lang/src/ caffeine_lang/test/ caffeine_cli/src/ caffeine_cli/test/ caffeine_lsp/src/ caffeine_lsp/test/ | while read num ; do
   clear
   echo "Changes detected, running tests..."
   echo "=================================="
-  cd caffeine_lang && gleam test "$@" && cd ../caffeine_lsp && gleam test "$@" && cd ../caffeine_cli && gleam test "$@" && cd ..
+  (cd caffeine_lang && gleam test "$@")
+  (cd caffeine_lsp && gleam test "$@")
+  (cd caffeine_cli && gleam test "$@")
 done
